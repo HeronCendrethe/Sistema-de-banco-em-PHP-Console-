21d3a242096cff4ee6c254313a0d993e64b27a85
HeronCendrethe <heron.cendrethe@gmail.com>
2021-07-08 09:24:58 -0300

    Estudo de Arrays Assosiativas em PHP

---
+++ vetor_PHP.php
@@ -0,0 +1,28 @@
+<?php 
+
+$conta1 = [
+    'Titular' => "João",
+    'CPF' => '123456789',
+    'DataNasc' => '123456',
+];
+
+$conta2 = [
+
+    'Titular' => "Maria",
+    'CPF' => '7894562655',
+    'DataNasc'=> '789456',
+];
+
+$conta3 = [
+
+    'Titular' => "Felipe",
+    'CPF' => '789962',
+    'DataNasc'=> '126842364',
+];
+
+
+    echo 'o Titular da conta 1 e: '.$conta1['Titular'].PHP_EOL;
+    echo 'o Titular da conta 2 e: '.$conta2['Titular'].PHP_EOL;
+    echo 'o Titular da conta 3 e: '.$conta3['Titular'].PHP_EOL;
+
+
